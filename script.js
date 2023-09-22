36abd9cda62f57afb3f10e116ccca182849fe428
Sammysiri <144451198+Sammysiri@users.noreply.github.com>
2023-09-22 08:01:20 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("worked")
+
+if (document.cookie.includes('darkmode=1')) {
+   var element = document.body;
+   element.classList.toggle('darkmode');
+}
+
+console.log("worked2")
+function darkmode() {
+   if (!document.cookie.includes('darkmode=1')) {
+      document.cookie = 'darkmode=1; expires=Tue, Jan 1 12:00:00 AM EST 2030';
+   } else {
+      document.cookie = 'darkmode=0; expires=Tue, Jan 1 12:00:00 AM EST 2030';
+   }
+   var element = document.body;
+   element.classList.toggle('darkmode');
+}
